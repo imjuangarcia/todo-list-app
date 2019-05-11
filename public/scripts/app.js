fdd62e9e8ba5c48b57c8e985745c3e294a35119c
Juan Martín García <hello@juangarcia.com.ar>
2019-05-11 10:57:25 -0300

    Added functionality to complete todos

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,43 +3,100 @@
 var appInfo = {
   title: "To-Do App",
   subtitle: "Next Up:",
-  options: ["One", "Two"]
+  toDos: []
 };
 
-var template = React.createElement(
-  "section",
-  { "class": "title" },
-  React.createElement(
-    "h1",
-    null,
-    appInfo.title
-  ),
-  appInfo.subtitle && React.createElement(
-    "p",
-    null,
-    appInfo.subtitle
-  ),
-  React.createElement(
-    "p",
-    null,
-    appInfo.options.length > 0 ? "Tasks for today:" : "No Tasks for today. Go get some sun! ☀️"
-  ),
-  React.createElement(
-    "ol",
-    null,
+var addToDo = function addToDo(e) {
+  e.preventDefault();
+  var toDo = e.target.elements.toDo.value;
+
+  if (toDo) {
+    appInfo.toDos.push(toDo);
+    e.target.elements.toDo.value = "";
+    renderToDos();
+  }
+};
+
+var makeDecision = function makeDecision() {
+  var randomNumber = Math.floor(Math.random() * appInfo.toDos.length);
+  var option = appInfo.toDos[randomNumber];
+  alert(option);
+};
+
+var completeToDo = function completeToDo(e) {
+  for (var i = 0; i < appInfo.toDos.length; i++) {
+    if (appInfo.toDos[i] === e.target.nextSibling.innerHTML) {
+      delete e.target.parentElement.remove();
+      return appInfo.toDos.splice(i, 1);
+    }
+  }
+  renderToDos();
+};
+
+var deleteToDos = function deleteToDos() {
+  appInfo.toDos = [];
+  renderToDos();
+};
+
+var renderToDos = function renderToDos() {
+  var template = React.createElement(
+    "section",
+    { className: "title" },
     React.createElement(
-      "li",
+      "h1",
       null,
-      "Item 1"
+      appInfo.title
+    ),
+    appInfo.subtitle && React.createElement(
+      "p",
+      null,
+      appInfo.subtitle
     ),
     React.createElement(
-      "li",
+      "p",
       null,
-      "Item 2"
+      appInfo.toDos.length > 0 ? "Tasks for today:" : "No Tasks for today. Go get some sun! ☀️"
+    ),
+    React.createElement(
+      "button",
+      { disabled: appInfo.toDos.length === 0, onClick: makeDecision },
+      "What should I do?"
+    ),
+    React.createElement(
+      "button",
+      { onClick: deleteToDos },
+      "Remove All ToDos"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      appInfo.toDos.map(function (toDo) {
+        return React.createElement(
+          "li",
+          { key: toDo },
+          React.createElement("input", { type: "checkbox", onClick: completeToDo }),
+          React.createElement(
+            "span",
+            null,
+            toDo
+          )
+        );
+      })
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: addToDo },
+      React.createElement("input", { type: "text", name: "toDo" }),
+      React.createElement(
+        "button",
+        null,
+        "Add ToDo"
+      )
     )
-  )
-);
+  );
+  ReactDOM.render(template, appRoot);
+};
 
 var appRoot = document.querySelector("#app");
 
-ReactDOM.render(template, appRoot);
+renderToDos();
--- src/app.js
+++ src/app.js
@@ -1,25 +1,74 @@
-let appInfo = {
+const appInfo = {
   title: "To-Do App",
   subtitle: "Next Up:",
-  options: ["One", "Two"]
+  toDos: []
 };
 
-let template = (
-  <section class="title">
-    <h1>{appInfo.title}</h1>
-    {appInfo.subtitle && <p>{appInfo.subtitle}</p>}
-    <p>
-      {appInfo.options.length > 0
-        ? "Tasks for today:"
-        : "No Tasks for today. Go get some sun! ☀️"}
-    </p>
-    <ol>
-      <li>Item 1</li>
-      <li>Item 2</li>
-    </ol>
-  </section>
-);
+const addToDo = e => {
+  e.preventDefault();
+  const toDo = e.target.elements.toDo.value;
 
-let appRoot = document.querySelector("#app");
+  if (toDo) {
+    appInfo.toDos.push(toDo);
+    e.target.elements.toDo.value = "";
+    renderToDos();
+  }
+};
 
-ReactDOM.render(template, appRoot);
+const makeDecision = () => {
+  const randomNumber = Math.floor(Math.random() * appInfo.toDos.length);
+  const option = appInfo.toDos[randomNumber];
+  alert(option);
+};
+
+const completeToDo = e => {
+  for (let i = 0; i < appInfo.toDos.length; i++) {
+    if (appInfo.toDos[i] === e.target.nextSibling.innerHTML) {
+      delete e.target.parentElement.remove();
+      return appInfo.toDos.splice(i, 1);
+    }
+  }
+  renderToDos();
+};
+
+const deleteToDos = () => {
+  appInfo.toDos = [];
+  renderToDos();
+};
+
+const renderToDos = () => {
+  const template = (
+    <section className="title">
+      <h1>{appInfo.title}</h1>
+      {appInfo.subtitle && <p>{appInfo.subtitle}</p>}
+      <p>
+        {appInfo.toDos.length > 0
+          ? "Tasks for today:"
+          : "No Tasks for today. Go get some sun! ☀️"}
+      </p>
+      <button disabled={appInfo.toDos.length === 0} onClick={makeDecision}>
+        What should I do?
+      </button>
+      <button onClick={deleteToDos}>Remove All ToDos</button>
+      <ol>
+        {appInfo.toDos.map(toDo => {
+          return (
+            <li key={toDo}>
+              <input type="checkbox" onClick={completeToDo} />
+              <span>{toDo}</span>
+            </li>
+          );
+        })}
+      </ol>
+      <form onSubmit={addToDo}>
+        <input type="text" name="toDo" />
+        <button>Add ToDo</button>
+      </form>
+    </section>
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+const appRoot = document.querySelector("#app");
+
+renderToDos();
