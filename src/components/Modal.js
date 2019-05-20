3ea50fa1bdcf4f34afc078ede374e09925eb52b8
Juan Martín García <hello@juangarcia.com.ar>
2019-05-20 15:09:30 -0300

    Styles to modal window

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,10 +6,17 @@ const OptionModal = props => (
     isOpen={!!props.selectedToDo}
     contentLabel="Selected ToDo"
     onRequestClose={props.clearSelectedToDo}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected ToDo</h3>
-    {props.selectedToDo && <p>{props.selectedToDo}</p>}
-    <button onClick={props.clearSelectedToDo}>Okay</button>
+    <h3 className="modal-title">
+      🤔 <br />
+      What should I do?
+    </h3>
+    {props.selectedToDo && <p className="modal-body">{props.selectedToDo}</p>}
+    <button className="modal-button" onClick={props.clearSelectedToDo}>
+      Let's do this!
+    </button>
   </Modal>
 );
 
