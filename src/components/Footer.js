5890cd3b22a220f7295a11944818e33d54aae6c0
Juan Martín García <hello@juangarcia.com.ar>
2019-06-08 09:38:23 -0300

    Added link to the repo on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,20 @@ const Footer = () => (
       Copyright &copy; <span>{new Date().getFullYear()}</span> JMG. Designed and
       Developed by{" "}
       <a href="https://www.juangarcia.design" target="_blank">
-        Juan Mart&iacute;n Garc&iacute;a
+        Juan Mart&iacute;n Garc&iacute;a.
+      </a>{" "}
+      Hosted on{" "}
+      <a href="https://www.netlify.com" target="_blank">
+        Netlify.
+      </a>
+    </p>
+    <p>
+      Wanna contribute?{" "}
+      <a
+        href="https://gitlab.com/jmg-side-projects/todo-list-app/issues"
+        target="_blank"
+      >
+        File a bug or request a feature
       </a>
     </p>
   </footer>
